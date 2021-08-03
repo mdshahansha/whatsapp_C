a9e45e022b0cc956f0898c0e5b8f4f8a079fb36e
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-08-03 21:29:20 +0530

    value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React,{useEffect} from 'react';
 import './App.css';
 import Sidebar from "./Sidebar"
 import Chat from './Chat'
+import Pusher from "pusher.js"
+import axios from './axios';
+import { response } from 'express';
+import { message } from 'statuses';
+
 function App() {
+  useEffect(() => {
+  axios.get('/messages/sync')
+  .then(response=>{
+    console.log(response.data);
+    setMessages(response.data);
+  })
+},[]);
+
+  useEffect(()=>{
+    const pusher = new Pusher('a8843da28186118dd425', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('inserted', (newMessages)=> {
+      alert(JSON.stringify(data));
+      setMessages([...messages],data)
+    });
+  },[messages]);
+
   return (
     <div className="app">
       <div className="app__body"> 
